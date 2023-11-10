dab0209bc979fe438d54c0a2b6f569859efd84f7
gelewaut <gelewaut@itba.edu.ar>
2023-11-10 11:33:26 -0300

    Api Funcional
    Co-authored-by: Francisco Dominguez <fdominguezm@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# tpo_bd2
+# TPO_BD2
+
+
+### Ejecutar la API
+
+#### Requerimientos:
+- Tener instalado node y postgres  
+- configurar los datos de la base de datos en el archivo db.js en la carpeta api
+los campos a configurar son:
+    - user: usuario de postgres
+    - password: contraseña de postgres
+    - database: nombre de la bd donde se encuentran las tablas (e01_cliente y e01_producto)
+    - host: localhost
+    - puerto: por default el de postgres es el 5432
+
+
+Ingresar a la carpeta api con el comando `cd ./api`  
+Ejecutar el comando `node index` y ya tendra corriendo la api en localhost:3000
+
+#### La Api Soporta:  
+Clientes:  
+- Post -> /clients con un cuerpo json con los atributos (nro_cliente, nombre, apellido, direccion,activo)
+- Put -> /clients/:nro_cliente con un cuerpo json con los atributos (nombre, apellido, direccion, activo)
+- Get -> /clients/:nro_cliente
+- Delete -> /clients/:nro_cliente  
+
+Productos:
+- Post -> /products con un cuerpo json con los atributos (codigo_producto, marca, nombre, descripcion, precio, stock)
+- Put -> /products/:codigo_producto con un cuerpo json con los atributos (marca, nombre, descripcion, precio, stock)
+- Get -> /products/:codigo_producto
--- api/index.js
+++ api/index.js
@@ -11,12 +11,13 @@ app.post("/clients", async(req, res) => {
     try {
         const {nro_cliente, nombre, apellido, direccion,activo } = req.body; 
         const newClient = 
-            await pool.query("INSERT INTO E01_CLIENTE (nro_cliente,nombre,apellido,direccion,activo)"+ 
+            await pool.query("INSERT INTO E01_CLIENTE (nro_cliente,nombre,apellido,direccion,activo) "+ 
             "VALUES ($1,$2,$3,$4,$5) RETURNING *", 
             [nro_cliente, nombre, apellido, direccion, activo]);
         res.json(newClient.rows[0]);
     } catch (err) {
-        console.error(err.message)
+        console.error(err.message);
+        res.json();
     }
 })
 
@@ -32,23 +33,25 @@ app.get("/clients/:nro_cliente", async (req, res) => {
         res.json(client.rows[0]);
     } catch (error) {
         console.error(error.message);
+        res.json();
     }
 })
 
 // Update Client
-app.put("clients/:nro_cliente", async (req, res) => {
+app.put("/clients/:nro_cliente", async (req, res) => {
     try {
         const {nro_cliente} = req.params;
         const { nombre, apellido, direccion, activo } = req.body; 
         const updateClient = await pool.query(
-            "update e01_cliente" +
-            "set (nombre,apellido,direccion,activo) = ($1,$2,$3,$4)" +
-            "where nro_cliente = $5 RETURNING *",
+            "update e01_cliente " +
+            "set (nombre,apellido,direccion,activo) = ($1,$2,$3,$4) " +
+            "where nro_cliente = $5 returning *",
             [nombre, apellido, direccion, activo, nro_cliente]
         )
         res.json(updateClient.rows[0]);
     } catch (err) {
         console.log(err.message);
+        res.json();
     }
 })
 
@@ -64,6 +67,7 @@ app.delete("/clients/:nro_cliente", async (req, res) => {
         res.json("CLIENTE ELIMINADO EXITOSAMENTE");
     } catch (error) {
         console.error(error.message);
+        res.json();
     }
 })
 
@@ -75,29 +79,31 @@ app.post("/products", async(req, res) => {
     try {
         const {codigo_producto, marca, nombre, descripcion, precio, stock } = req.body; 
         const newProduct = 
-            await pool.query("INSERT INTO e01_producto (codigo_producto, marca, nombre, descripcion, precio, stock)"+ 
+            await pool.query("INSERT INTO e01_producto (codigo_producto, marca, nombre, descripcion, precio, stock) "+ 
             "VALUES ($1,$2,$3,$4,$5,$6) RETURNING *", 
             [codigo_producto, marca, nombre, descripcion, precio, stock]);
         res.json(newProduct.rows[0]);
     } catch (err) {
-        console.error(err.message)
+        console.error(err.message);
+        res.json();
     }
 })
 
 // Update Product
-app.put("products/:codigo_producto", async (req, res) => {
+app.put("/products/:codigo_producto", async (req, res) => {
     try {
         const {codigo_producto} = req.params;
         const { marca, nombre, descripcion, precio, stock } = req.body; 
         const updateClient = await pool.query(
-            "update e01_producto" +
-            "set (marca, nombre, descripcion, precio, stock) = ($1,$2,$3,$4,$5)" +
+            "update e01_producto " +
+            "set (marca, nombre, descripcion, precio, stock) = ($1,$2,$3,$4,$5) " +
             "where codigo_producto = $6 RETURNING *",
             [marca, nombre, descripcion, precio, stock, codigo_producto]
         )
         res.json(updateClient.rows[0]);
     } catch (err) {
         console.log(err.message);
+        res.json();
     }
 })
 
@@ -113,21 +119,7 @@ app.get("/products/:codigo_producto", async (req, res) => {
         res.json(client.rows[0]);
     } catch (error) {
         console.error(error.message);
-    }
-})
-
-// Delete a client
-app.delete("/products/:codigo_producto", async (req, res) => {
-    try {
-        const { codigo_producto } = req.params;
-        const deleteClient = 
-            await pool.query(
-                "DELETE FROM e01_producto WHERE codigo_producto = $1 ",
-                [codigo_producto]);
-
-        res.json("PRODUCTO ELIMINADO EXITOSAMENTE");
-    } catch (error) {
-        console.error(error.message);
+        res.json();
     }
 })
 
